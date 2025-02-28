2476cd9cdca82b048ecddb6d2383ff41e45755a0
JeremyBarrera <112196500+JeremyBarrera@users.noreply.github.com>
2025-02-28 20:51:40 +0000

    First commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,97 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const [items, setItems] = useState([]);
+  const [text, setText] = useState("");
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const getTodos = async () => {
+    try {
+      const response = await fetch("https://playground.4geeks.com/todo/users/JeremyBarrera");
+      if (response.status == 404) {
+        await createUser();
+        return;
+      }
+      const data = await response.json();
+      setItems(data.todos)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const createUser = async () => {
+    try {
+      const response = await fetch("https://playground.4geeks.com/todo/users/JeremyBarrera", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+      });
+      if (response.status === 201) {
+        await getTodos();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const addItem = async (e) => {
+    if (e.key === "Enter" && text.trim() !== "") {
+      try {
+        const response = await fetch("https://playground.4geeks.com/todo/todos/JeremyBarrera", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            "label": text,
+            "is_done": false,
+          }),
+        });
+        if (response.status === 201) {
+          await getTodos();
+          setText("");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  const deleteItem = async (id) => {
+    try {
+      const response = await fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+      });
+      if (response.status === 204) {
+        await getTodos();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  return (
+    <div className="box">
+      <input
+        className="list"
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        onKeyDown={addItem}
+        placeholder="What needs to be done?"
+      />
+      <ul className="list-container">
+        {items.map((todo, index) => (
+          <li key={index} className="list-item">
+            {todo.label}
+            <button className="fa-solid fa-x delete" onClick={() => deleteItem(todo.id)}>X</button>
+          </li>
+        ))}
+      </ul>
+      <div className="counter">Total Items: {items.length}</div>
+    </div>
+  );
 };
 
 export default Home;
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -13,6 +13,7 @@ import Home from './components/Home';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <h1 className="title">Todo</h1>
     <Home/>
   </React.StrictMode>,
 )
